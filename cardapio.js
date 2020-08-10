0dca713acc892788770bd7238aefa5daa5b0e3f9
Carla Scherer <carlatscherer@gmail.com>
2020-08-10 10:49:09 -0300

    criando a estrutura de servidor do projeto

---
+++ cardapio.js
@@ -0,0 +1,20 @@
+const cardapio = [
+    {
+        sabor: "Marguerita",
+        preco: 35
+    },
+    {
+        sabor: "Calabresa",
+        preco: 40
+    },
+    {
+        sabor: "Bacon",
+        preco: 37
+    },
+    {
+        sabor: "Coração",
+        preco: 29
+    }
+];
+
+module.exports = cardapio;
